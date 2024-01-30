3b634994a3e3849ac3d4b9f14e1dce86b43abb90
laurac1910 <lauracordero0806@gmail.com>
2024-01-30 06:09:51 -0800

    sprint1

--- src/main/java/com/mindhub/homebanking/HomebankingApplication.java
+++ src/main/java/com/mindhub/homebanking/HomebankingApplication.java
@@ -1,7 +1,11 @@
 package com.mindhub.homebanking;
 
+import com.mindhub.homebanking.models.Client;
+import com.mindhub.homebanking.repositories.ClientRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HomebankingApplication {
@@ -9,5 +13,16 @@ public class HomebankingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HomebankingApplication.class, args);
 	}
+@Bean
+	 public CommandLineRunner data (ClientRepository clientRepository){
+
+		return args -> {
+			Client laura = new Client("Melba", "Morel", "melba@mindhub.com");
+
+			clientRepository.save(laura);
+
+			System.out.println(laura);
+		};
+	 }
 
 }
